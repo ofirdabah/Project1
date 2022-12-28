f5b00c31a579cde23f7c774f5d9ce6379df52ed3
Ofir Dabah <dabahof@mta.ac.il>
2022-12-28 10:11:19 +0200

    STL

--- Project1/Date.cpp
+++ Project1/Date.cpp
@@ -1,19 +1,19 @@
 #include "Date.h"
-//
-//void Date::setday(int d)throw (DayRange)
-//{
-//	if ((d < 1) || (d > 31))
-//		throw DayRange();
-//	day = d;
-// 
-// 
-//}
-//void Date::setmonth(int m)throw (MonthRange)
-//{
-//	if ((m < 1) || (m > 12))
-//		throw MonthRange();
-//	month = m;
-//}
+
+void Date::setday(int d)throw (DayRange)
+{
+	if ((d < 1) || (d > 31))
+		throw DayRange();
+	day = d;
+ 
+ 
+}
+void Date::setmonth(int m)throw (MonthRange)
+{
+	if ((m < 1) || (m > 12))
+		throw MonthRange();
+	month = m;
+}
 void Date::setyear(int y) throw (YearRange)
 {
 	if ((y < 1900) || (y > 2022))
